2f31bc03e77111a324e7075c863f46553140ce52
lucamariotti <luca.mariotti3@gmail.com>
2023-09-15 22:58:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,21 +17,15 @@ credentials = service_account.Credentials.from_service_account_info(
         "https://www.googleapis.com/auth/spreadsheets",
     ],
 )
+
 conn = connect(credentials=credentials)
-
-@st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
 sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.Data} has a :{row.Mese}:")
-    
+# Query the Google Sheet
+query = f'SELECT * FROM "{sheet_url}"' 
+data = pd.read_sql_query(query, conn)
+
+
 # Convert "Date" column to datetime format
 data['Data'] = pd.to_datetime(data['Data'],format="%d/%m/%Y")
 
